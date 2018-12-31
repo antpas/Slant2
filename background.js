85ce5a7ce0da4f7df828a8317e7ea420103b9b0c
Anthony Pasquariello <antpas@bu.edu>
2018-12-31 16:01:37 -0500

    Update background.js

--- background.js
+++ background.js
@@ -1,34 +1,47 @@
 //handling url pull from each active chrome tab
-chrome.tabs.onUpdated.addListener(function(id,activeInfo,tab) 
+chrome.tabs.onUpdated.addListener(function(id,activeInfo,tab)
 {
-	if(activeInfo.status != ("complete"))
-		return;
+
 
   	var tablink = tab.url;
-    
+
     console.log(tablink.substring(0,4)); //display url on console
 
      	if (tablink.substring(0,4) == 'http')
      	{
      		var request = new XMLHttpRequest(); // initiate http request
 
-		// Open a new connection, use GET method on the api endpoint
-		request.open('POST', 'https://3xe435ebm9.execute-api.us-east-2.amazonaws.com/Dev/classifyurl', true);
-		
-		var temp = '{"data": "'+tablink+'"}' //append url in a new variable for expected format
+				// Open a new connection, use GET method on the api endpoint
+				request.open('POST', 'https://3xe435ebm9.execute-api.us-east-2.amazonaws.com/Dev/classifyurl', true);
 
-		var payload = JSON.parse(temp)
+				var temp = '{"data": "'+tablink+'"}' //append url in a new variable for expected format
 
-		console.log(payload);
+				var payload = JSON.parse(temp)
 
-		var output = request.send(temp);
+				console.log(payload);
 
-		request.onreadystatechange = function() 
-		{ 
-			var out = request.responseText;
-			console.log(out);
-			document.querySelector('.classifyText').innerHTML = out; //current way to display labels in popup
-		};
+				var output = request.send(temp);
+
+				request.onreadystatechange = function()
+				{
+					//Wow this is ugly
+					var out = request.responseText;
+					out = out.substr(1).slice(0, -1);
+					out = out.replace(/u'(?=[^:]+')/g, "'")
+					out = JSON.parse(out);
+					//console.log(out);
+					var prob = out.prob
+					prob = Math.trunc(prob[0] * 100)
+					console.log(prob)
+					var party = out.label
+					party = party[0]
+					party = party.replace("__label__", "")
+					console.log(party)
+					var x = document.getElementById("SlantPopup");
+					x.querySelector('.score').innerHTML = prob + "%"; //current way to display labels in popup
+					var y = document.getElementById("SlantPopup2");
+					y.querySelector('.party').innerHTML = party;
+				};
 	    }
 
-}); 
+});
